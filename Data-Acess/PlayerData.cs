ee7add280cd2392ca84478d4c89fd44b8a4deac5
Kirbo <grandadmiralpancake@gmail.com>
2022-03-13 21:37:12 -0400

    Consolodating Loaders, added TODO

--- Data-Acess/PlayerData.cs
+++ Data-Acess/PlayerData.cs
@@ -44,4 +44,32 @@ namespace Basiverse
         }
     
     }
+
+    class Saver{
+    /*
+        Data Structure:-
+        Save Name
+        Other metadata?
+        Then use Binary Data Writer to write player OBJ      
+    */
+
+        private string [] Locations;
+
+        public Saver(){
+            Locations = new string[3];
+            Locations[0] = Directory.GetCurrentDirectory() + "\\save1\\";
+            Locations[1] = Directory.GetCurrentDirectory() + "\\save2\\";
+            Locations[2] = Directory.GetCurrentDirectory() + "\\save3\\";
+        }
+
+        public int SaveData(int index, string saveName, Player outPlayer){
+            string outLocation = Locations[index] + "metadata.data";
+            using (StreamWriter sw = new StreamWriter(outLocation)){
+                sw.WriteLine(saveName);
+            }
+            string binLocation = Locations[index] + "savedata.bin";
+            BinarySerialization.WriteToBinaryFile<Player>(binLocation, outPlayer);
+            return 1;
+        }
+    }
 }
