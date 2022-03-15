9c1047f3f2b13c66abc65c1b8d5e46a88cddde07
Kirbo <grandadmiralpancake@gmail.com>
2022-03-14 23:45:57 -0400

    Data objects and basic mapgen sketchout

--- Map/Map.cs
+++ Map/Map.cs
@@ -7,6 +7,30 @@ using System.Collections.Generic;
 This will contain the Map object that contains all of the Location Objects
 */
 namespace Basiverse{
+    class Map{ // Map contains the Location nodes
+        List<Location> AllNodes;
+    }
 
-    
+    class Location{ // Locations contain names, descriptors and maybe points of interest
+        string Name;
+        string Description;
+        List<PointofInterest> Interests;
+        List<Location> NearbyNodes;
+    }
+
+    class PointofInterest{ // Point of Interest Objects that contain descriptors, names, etc
+        string Name;
+        string Description;
+
+        /* Possible Additions - 
+           NPCs
+           Merchant Vessels
+           Bases
+           Mining Colonies
+           Warships
+           Pirates
+           etc.
+        */ 
+
+    }
 }
---
+++ Tools/MapGenerator.cs
@@ -0,0 +1,14 @@
+using System;
+using Basiverse;
+using System.Collections.Generic;
+
+// TODO Generate a random map based on data
+namespace Basiverse{
+    class MapGen{
+
+        public Map Generate(){
+            Map outMap = new Map();
+            return outMap; // Temp
+        }
+    }
+}
